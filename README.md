074a11276953f963521954c27dc1cefdf9af8d4b
Timothée Ménage <tim.menage@gmail.com>
2017-02-09 17:07:38 +0100

    Still some work to do

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+ Setup
+ 
+ npm i -g http-server
+ 
+ Launche before calling web page:
+ 
+ http-server
+ 
+ A lancer dans le dossier SRC
+ 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
-
 function music32FloatArray (real) {
+  console.log("1");
   var audioContext = new AudioContext();
   var osc = audioContext.createOscillator();
   var imag = new Float32Array(real.length);
+  console.log("2-0");
   var hornTable = audioContext.createPeriodicWave(real, imag);
+  console.log("2-1");
 
   osc = audioContext.createOscillator();
   osc.setPeriodicWave(hornTable);
   osc.frequency.value = 80;
   osc.connect(audioContext.destination);
   osc.start(0);
+  console.log("3");
 }
+
+$.get('/sun.csv')
+  .done(function(data) {
+    //console.log(data);
+
+    let tab = data.split('\n').map(line => line.split(','));
+    tab.shift();
+    const floatTab = tab.map(line => line.map(cell => parseFloat(cell)));
+
+
+
+    music32FloatArray(floatTab[0]);
+  })
+  .fail(function() {
+    console.log('fail');
+  });
