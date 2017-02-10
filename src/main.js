0b713b174bbb01d0280ee086310a88acd2d55310
aloisdg <aloisdegouvello@live.fr>
2017-02-10 17:23:18 +0100

    test interval

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
+  var osc = audioContext.createOscillator();
+
+var varCounter = 0;
+var varName = function(list){
+     if(varCounter <= list) {
+          varCounter++;
+        osc.frequency.value = list[varCounter];
+     } else {
+          clearInterval(varName);
+     }
+};
+
 function music32FloatArray (floatList) {
   var audioContext = new AudioContext();
-  var osc = audioContext.createOscillator();
   var real = new Float32Array(floatList);
   var imag = new Float32Array(real.length);
   var hornTable = audioContext.createPeriodicWave(real, imag);
 
   osc = audioContext.createOscillator();
   osc.setPeriodicWave(hornTable);
-  osc.frequency.value = 80;
+
+setInterval(varName(floatList), 1000);
+
+
+
   osc.connect(audioContext.destination);
   osc.start(0);
 }
